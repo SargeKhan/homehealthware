c35ad190b49ce55adcbece4bbfce282b34a286f8
SargeKhan <10beseukhan@seecs.edu.pk>
2016-02-18 01:48:54 +0500

    just verifing

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  db: 'mongodb://localhost/your_project_development',
+  db: 'mongodb://localhost/homehealthware-dev',
   facebook: {
     clientID: 'APP_ID',
     clientSecret: 'SECRET',
